58dfd2d70bb31546f0a4cc94e2caa21c3357f2e5
Sunny <pandeysunny01@gmail.com>
2024-05-07 22:37:03 +0530

    updated react login page

---
+++ react/react-login/src/App.tsx
@@ -0,0 +1,99 @@
+import "./App.css";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { toast } from "@/components/ui/use-toast";
+
+const FormSchema = z.object({
+  username: z.string().min(1, {
+    message: "Username cannot be blank.",
+  }),
+  password: z.string().min(1, {
+    message: "Password cannot be blank.",
+  }),
+});
+
+function App() {
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+    defaultValues: {
+      username: "",
+      password: "",
+    },
+  });
+
+  function onSubmit(data: z.infer<typeof FormSchema>) {
+    toast({
+      title: "You submitted the following values:",
+      description: (
+        <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
+          <code className="text-white">{JSON.stringify(data, null, 2)}</code>
+        </pre>
+      ),
+    });
+  }
+  return (
+    <>
+      <div className="w-full h-full px-96 py-10 ">
+        <div className="px-52 p-40">
+          <Form {...form}>
+            <form
+              onSubmit={form.handleSubmit(onSubmit)}
+              className="border-lime-100 border-2 p-5 rounded-2xl space-y-6 "
+            >
+              <FormField
+                control={form.control}
+                name="username"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Username</FormLabel>
+                    <FormControl>
+                      <Input placeholder="username" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Password</FormLabel>
+                    <FormControl>
+                      <Input
+                        type="password"
+                        placeholder="password"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <Button
+                type="submit"
+                className="bg-gradient-to-r from-gray-400 to-blue-500 hover:from-emerald-500 hover:to-indigo-500"
+              >
+                Submit
+              </Button>
+            </form>
+          </Form>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
